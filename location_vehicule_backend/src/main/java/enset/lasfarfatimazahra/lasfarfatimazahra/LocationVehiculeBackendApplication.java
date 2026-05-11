a399afde9dddd8be858c0177ffcb7a5ee01b55dc
Fatima Zahra Lasfar <fatimazahralasfar2209@gmail.com>
2026-05-11 14:59:25 +0100

    Initial project structure

---
+++ location_vehicule_backend/src/main/java/enset/lasfarfatimazahra/lasfarfatimazahra/LocationVehiculeBackendApplication.java
@@ -0,0 +1,13 @@
+package enset.lasfarfatimazahra.lasfarfatimazahra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationVehiculeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationVehiculeBackendApplication.class, args);
+    }
+
+}
---
+++ location_vehicule_backend/src/test/java/enset/lasfarfatimazahra/lasfarfatimazahra/LocationVehiculeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.lasfarfatimazahra.lasfarfatimazahra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationVehiculeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
